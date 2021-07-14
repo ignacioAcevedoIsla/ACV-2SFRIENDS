60037d8922cb3458059f393c169a45724283f0a0
ignacioAcevedoIsla <75747465+ignacioAcevedoIsla@users.noreply.github.com>
2021-07-14 06:53:56 -0400

    Update header.php

--- views/layout/header.php
+++ views/layout/header.php
@@ -7,7 +7,7 @@
   <html lang="es">
   	<head>
   		<meta charset="utf-8" />
-  		<title>ACV2'S FRIENDS</title>
+  		<title>ACV2'S FRIENDS beta</title>
 
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   		<link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
